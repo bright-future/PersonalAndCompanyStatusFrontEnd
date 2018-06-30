c8344fd0c2e08af8d0de4ed4ed7103c2b8dd8ee5
Rahul Khanna <rahul.khanna@naukri.com>
2018-06-30 19:10:05 +0530

    a successful post request

--- my-app/src/app/services/user-comparison.service.ts
+++ my-app/src/app/services/user-comparison.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@angular/core';
 import { CompaniesListOfGivenRange } from '../configFiles/CompaniesListOfGivenRange';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+// import { catchError, map, tap } from 'rxjs/operators';
 
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded',
+                              'Access-Control-Allow-Origin':'*'})
 };
 
 @Injectable({
@@ -18,10 +22,31 @@ export class UserComparisonService {
   constructor(private http: HttpClient) { }
 
   getCompaniesListOfGivenRange(companies: CompaniesListOfGivenRange): Observable<any> {
-    return this.http.post(this.url, companies, httpOptions);
-  }
+    let body = new FormData();
 
-  handleError(): void{
+    body.append('college', "118");
+    body.append('branch', "12");
+    body.append('startBatch', "2000");
+    body.append('endBatch', "2013");
+
+    console.log("sending request",body,this.url,httpOptions);
+    return this.http.post(this.url, body)
+    map((res: Response) => res.setHeader('Access-Control-Allow-Origin','http://192.168.173.56:8092  '))
+    .catch((error:any)=> Observable.throw(error.json().error||"server error"))
+    .subscribe();
+    // .map((response: Response) => {
+    //     console.log("response",response);
+    //     return response;
+    //   });
+
+  }
+  extractData(res:any){
+    console.log("requested");
+    console.log(res);
+    console.log(res.body());
+    return res;
+  }
+  handleError(res:any): void{
     console.log("Error in sending request");
   }
 
--- my-app/src/app/user-comparison/user-comparison.component.ts
+++ my-app/src/app/user-comparison/user-comparison.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserComparisonService } from '../services/user-comparison.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CompaniesListOfGivenRange } from '../configFiles/CompaniesListOfGivenRange';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-user-comparison',
@@ -15,6 +17,7 @@ export class UserComparisonComponent implements OnInit {
   constructor(private userComparisonService: UserComparisonService) { }
 
   ngOnInit() {
+    console.log("init");
     this.request = new CompaniesListOfGivenRange();
     this.request.college="118";
     this.request.branch = "12";
@@ -22,9 +25,24 @@ export class UserComparisonComponent implements OnInit {
     this.request.endBatch = "2013";
     this.getData(this.request);
   }
-
-  getData(request: CompaniesListOfGivenRange): void{
-    this.userComparisonService.getCompaniesListOfGivenRange(request);
+  ngAfterViewInit(){
+    // console.log("init");
+    // this.request = new CompaniesListOfGivenRange();
+    // this.request.college="118";
+    // this.request.branch = "12";
+    // this.request.startBatch = "2000";
+    // this.request.endBatch = "2013";
+    // this.getData(this.request);
   }
-
+  getData(request: CompaniesListOfGivenRange): void{
+    this.userComparisonService.getCompaniesListOfGivenRange(request)
+          .subscribe(
+          result => console.log("component",result),
+          error => this.errorMessage = <any>error
+      );
+      console.log("init called");
+  }
+  extractData(res:any){
+    console.log("extractData="+res);
+  }
 }
